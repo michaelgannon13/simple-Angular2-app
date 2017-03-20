9ee672427c1447e29bf6204a3af3757062aee8cb
Michael gannon <michael.gannon13@gmail.com>
2017-03-20 17:44:08 +0000

    app
    all files

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+//importing component decorator from angular
+import {Component} from 'angular2/core';
+//./ means search from the CURRENT directory, not back one
+import {CoursesComponent} from './courses.component';
+import {AuthorsComponent} from './author.component';
+
+
+//calling the component which is an object 
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hello Angular</h1>
+               <courses></courses>
+               <authors></authors>`,
+    //adding a directive to extend DOM functionality
+    directives: [CoursesComponent, AuthorsComponent]
+})
+//AppComponant is root of our app
+export class AppComponent { }
